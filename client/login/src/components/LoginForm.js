4f847ed86647b07e49664429a16229a815893fd4
Adamkadaban <adamkadaban@gmail.com>
2022-12-02 08:45:49 -0500

    modified password field to be hidden

--- client/login/src/components/LoginForm.js
+++ client/login/src/components/LoginForm.js
@@ -20,10 +20,6 @@ function LoginForm({login, error}) { //functional component
             <h2> Login </h2>
             { /*Error*/ }
             <div className= "form-group">
-                <label htmlFor="name"> Name: </label>
-                <input type="text" name="name" id="name" onChange={ e => setDetails({...details, name: e.target.value})} value = {details.name}  />
-            </div>
-            <div className= "form-group">
                
                 <label htmlFor="email"> Email: </label>
                 { (details.email.includes("gmail") ) ? (<label htmlFor="message"> lookin good icon (gmail detected) </label> ) : ( console.log ("okay")) }
@@ -32,7 +28,7 @@ function LoginForm({login, error}) { //functional component
             </div>
             <div className= "form-group">
                 <label htmlFor="password"> Password: </label>
-                <input type="text" name="password" id="password" onChange={ e => setDetails({...details, password: e.target.value})} value = {details.password} />
+                <input type="password" name="password" id="password" onChange={ e => setDetails({...details, password: e.target.value})} value = {details.password} />
             </div>
             <input type= "submit" value = "LOGIN" />
         </div>
